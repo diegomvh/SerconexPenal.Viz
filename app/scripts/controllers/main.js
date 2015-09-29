1cf373e70f9dbfb26d5a55c70866429046a0e54a
Diego van Haaster <diegomvh@gmail.com>
2015-09-29 17:44:06 -0300

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name serconexPenalvizApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the serconexPenalvizApp
+ */
+angular.module('serconexPenalvizApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
