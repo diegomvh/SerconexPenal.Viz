f9699262a00a0a485a5b20af82c30be2dbdd54af
Diego van Haaster <diegomvh@gmail.com>
2015-09-30 08:28:44 -0300

    Puliendo los datos

--- scripts/data.py
+++ scripts/data.py
@@ -12,38 +12,12 @@ causas = [ "habilitado", "causa_id", "tipo", "desde", "hasta", "habilitante",
 # prefuse
 # NetworkX
 # Circos
-# Cambiar "Error de login," y "Fuera de tiempo," tiene un coma y es mi separador de lineas
-
 if __name__ == '__main__':
-    auditorias_docs = []
-    with open("auditoria.csv", "r") as f:
-        for linea in f.read().splitlines(False):
-            tokens = linea.split(',')
-            if "Requerimiento" in linea:
-                doc = { "documento": tokens[0], "localidad": tokens[1],
-                    "roles": tokens[2], "ultima_actividad": tokens[3],
-                    "actividad": tokens[4], "accion": tokens[5], 
-                    "modelo": tokens[6], "objeto_id": tokens[7], 
-                    "viejo_estado": tokens[8], "nuevo_estado": ",".join(tokens[9:-5]),
-                    "momento": tokens[-5], "user_agent": tokens[-4], 
-                    "ip": tokens[-3], "url": tokens[-2], "url_referrer": tokens[-1] 
-                    }
-            elif "MensajeLog" in linea:
-                doc = { "documento": tokens[0], "localidad": tokens[1],
-                    "roles": tokens[2], "ultima_actividad": tokens[3],
-                    "actividad": tokens[4], "accion": tokens[5], 
-                    "modelo": tokens[6], "objeto_id": tokens[7], 
-                    "viejo_estado": tokens[8], "nuevo_estado": ",".join(tokens[9:-6]),
-                    "momento": tokens[-5], "user_agent": tokens[-4], 
-                    "ip": tokens[-3], "url": tokens[-2], "url_referrer": tokens[-1] 
-                }
-            else:
-                doc = {t[0]: t[1] for t in zip(auditorias, tokens)}
-            print(linea, doc)
-            doc["momento"] = datetime.strptime(doc["momento"], "%Y-%m-%d %H:%M:%S.%f")
-            auditorias_docs.append(doc)
-        print(auditorias_docs) 
-    with open("causas.csv", "r") as f:
+    with open("../data/auditoria.csv", "r") as f:
+        docs = [ {t[0]: t[1] for t in zip(auditorias, linea.split(','))} \
+            for linea in f.read().splitlines(False) ]
+        print(len(docs))
+    with open("../data/causas.csv", "r") as f:
         docs = [ {t[0]: t[1] for t in zip(causas, linea.split(','))} \
             for linea in f.read().splitlines(False) ]
         print(len(docs))
